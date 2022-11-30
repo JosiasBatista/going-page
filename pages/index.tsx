4ae7c13b9aeadf8878c9f6b2bbfaf971e965374a
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2022-11-30 18:08:35 -0300

    Iniciando construção de Parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { Parallax } from 'react-scroll-parallax';
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,17 +15,106 @@ export default function Home() {
 
       <main>
         <div className={styles.pageHeader}>
-          <h1 className={styles.appName}>GOING</h1>
+          {/* <h1 className={styles.appName}>GOING</h1> */}
           
-          <Image
+          <Parallax className={styles.parallax} speed={-20}>
+            <Image
             priority
-            src="/images/landscape.png"
+            src="/images/Layer9.svg"
             className={styles.backgroundImage}
             alt=""
             layout="fill"
           />
-
+          </Parallax>
+          <Parallax className={styles.parallax} speed={-55}>
+            <Image
+            priority
+            src="/images/Layer10.svg"
+            className={styles.backgroundImage}
+            alt=""
+            layout="fill"
+          />
+          </Parallax>
+          <Parallax className={styles.parallax} speed={-15}>
+            <Image
+            priority
+            src="/images/Layer8.svg"
+            className={styles.backgroundImage}
+            alt=""
+            layout="fill"
+          />
+          </Parallax>
+          <Parallax className={styles.parallax} speed={0}>
+            <Image
+            priority
+            src="/images/Layer7.svg"
+            className={styles.backgroundImage}
+            alt=""
+            layout="fill"
+          />
+          </Parallax>
+          
+          <Parallax className={styles.parallax} speed={12.5}>
+            <Image
+            priority
+            src="/images/Layer6.svg"
+            className={styles.backgroundImage}
+            alt=""
+            layout="fill"
+          />
+          </Parallax>
+          <Parallax className={styles.parallax} speed={10}>
+            <Image
+              priority
+              src="/images/Layer5.svg"
+              className={styles.backgroundImage}
+              alt=""
+              layout="fill"
+            />
+          </Parallax>
+          
+          <Parallax className={styles.parallax} speed={10}>
+            <Image
+              priority
+              src="/images/Layer4.svg"
+              className={styles.backgroundImage}
+              alt=""
+              layout="fill"
+            />
+          </Parallax>
+          
+          <Parallax className={styles.parallax} speed={15}>
+            <Image
+              priority
+              src="/images/Layer3.svg"
+              className={styles.backgroundImage}
+              alt=""
+              layout="fill"
+            />
+          </Parallax>
+          
+          <Parallax className={styles.parallax} speed={15}>
+            <Image
+              priority
+              src="/images/Layer2.svg"
+              className={styles.backgroundImage}
+              alt=""
+              layout="fill"
+            />
+          </Parallax>
+          
+          <Parallax className={styles.parallax} speed={20}>
+            <Image
+              priority
+              src="/images/Layer1.svg"
+              className={styles.backgroundImage}
+              alt=""
+              layout="fill"
+            />
+          </Parallax>
         </div>
+
+        <div className={styles.pageRest} />
       </main>
     </div>
   )
