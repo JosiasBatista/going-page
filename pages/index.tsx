1b3df1f680d70a6c05bbbfd497a13f247f64ccd3
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2022-12-04 15:12:37 -0300

    Creating app description

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import ParallaxComponent from '../components/parallax';
 import FeatureCard from '../components/featureCard';
+import { MdHiking, MdAttractions, MdGroup } from 'react-icons/md'
 
 import WriteDiary from '../public/animations/write-diary.json';
 import FindDestination from '../public/animations/find-destination.json';
@@ -45,6 +46,27 @@ export default function Home() {
             />
           </section>
 
+          <div className={styles.pageSection}>
+            <span className={styles.sectionTitle}>Encontre outros amantes de viagem como você!</span>
+
+            <div className={styles.communityDescCont}>
+              <section className={`${styles.communityDesc} ${styles.firstDesc}`}>
+                <MdHiking size={120} color="#179DC7" />
+                <p className={styles.communityDescInfo}>Revele para o mundo as suas melhores experiências de viagem e quais atividades o mundo precisa conhecer!</p>
+              </section>
+
+              <section className={`${styles.communityDesc} ${styles.secDesc}`}>
+                <MdAttractions size={120} color="#179DC7" />
+                <p className={styles.communityDescInfo}>Descreva os lugares mais perfeitos que você conheceu e veja quem mais concorda com a sua opinão!</p>
+              </section>
+
+              <section className={`${styles.communityDesc} ${styles.thirdDesc}`}>
+                <MdGroup size={120} color="#179DC7" />
+                <p className={styles.communityDescInfo}>Faça parte de uma comunidade de pessoas que sabem que a vida é feita de experiências e compartilhar elas é muito melhor</p>
+              </section>
+            </div>
+
+          </div>
         </div>
       </main>
     </div>
